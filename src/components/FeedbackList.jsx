8e9e6d52fc230ce4b9ee839ed0834190e0edac47
Ezz4 <zuryta92@gmail.com>
2023-06-14 17:04:31 -0600

    including animation of fade out

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,10 +1,34 @@
 import FeedbackItem from "./FeedbackItem"
 import PropTypes from 'prop-types'
+import {motion, AnimatePresence} from 'framer-motion'
 
 function FeedbackList({feedBack, handleDelete}) {
     if(!feedBack || feedBack.length === 0){
         return <p>No feedBack Yet</p>
     }
+  
+    return (
+      <div className='feedback-list'>
+        <AnimatePresence>
+        {feedBack.map((item) =>(
+          <motion.div
+          key={item.id}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+          >
+            <FeedbackItem key= 
+            {item.id} 
+            item={item} 
+            handleDelete= {handleDelete}/>
+          </motion.div>          
+        ))}
+        </AnimatePresence>
+        
+      </div>
+    )
+
+  /*  without animation version
   return (
     <div className='feedback-list'>
       {feedBack.map((item) =>(
@@ -14,7 +38,7 @@ function FeedbackList({feedBack, handleDelete}) {
         handleDelete= {handleDelete}/>
       ))}
     </div>
-  )
+  )*/
 }
 
 FeedbackList.propTypes = {
