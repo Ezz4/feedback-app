1e60d9bf09eafc24376a5aed91acb40ef3c73add
Ezz4 <zuryta92@gmail.com>
2023-06-16 18:55:22 -0600

    Edit funtion added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import { useState } from "react"
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from "./components/Header"
-import FeedbackData from "./Data/FeedbackData"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
-import {v4 as uuidv4} from 'uuid'
 import AboutPage from "./Pages/AboutPage"
 import AboutIconLink from "./components/AboutIconLink"
 import { FeedbackProvider} from "./context/FeedbackContext"
 
 function App(){
-    const [feedback, setFeedback] = useState(FeedbackData)
-    const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure ypu want tp delete?')){
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }        
-    }
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
-    }
+   
     return(
         <FeedbackProvider>
         <Router>
@@ -29,8 +17,9 @@ function App(){
                 <Routes>
                 <Route exact path='/' element={
                     <>
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackList handleDelete={deleteFeedback}/> 
+                <FeedbackForm />
+                <FeedbackStats/>
+                <FeedbackList/> 
                     </>
                 }>
                 
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,6 +1,8 @@
 import { createContext, useState } from "react";
-
+import {v4 as uuidv4} from 'uuid'
 const FeedbackContext = createContext()
+
+
 
 export const FeedbackProvider = ({children}) => {
 
@@ -11,10 +13,46 @@ export const FeedbackProvider = ({children}) => {
             rating: 10
         },
     ])
+    
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    })
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
+
+    const updateFeedback = (id, updItem) => {
+    setFeedback(
+        feedback.map((item) => (item.id === id ?{...item, ...updItem} 
+            : item)))
+    }
+
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure ypu want tp delete?')){
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }        
+    }
+
+    const editFeedback = (item) =>{
+        setFeedbackEdit({
+            item,
+            edit: true
+        })
+    }
 
     return <FeedbackContext.Provider value={{
         feedback,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        feedbackEdit,
+        updateFeedback,
     }}>
+
     {children}
     </FeedbackContext.Provider>
 }
